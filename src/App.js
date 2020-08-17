307a248445957732aa9a0cd9ff75c9d29bffbe18
raiguard <caleb.heuer@gmail.com>
2020-08-17 12:12:19 -0600

    Check session for signed in user (needs endpoint)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.scss";
+import { setUser } from "./redux/reducer";
 
 function App() {
-  return <div className="App">Hello Aaron!</div>;
+  const [sessionChecked, setSessionChecked] = useState(false);
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.user);
+
+  // check session for signed in status
+  useEffect(() => {
+    const checkSession = async () => {
+      // TODO Aaron set up endpoint
+      // const res = await Axios.get("/auth/me");
+      const res = {};
+      if (res.data) {
+        dispatch(setUser(res.data));
+      }
+      setSessionChecked(true);
+    };
+    checkSession();
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      {sessionChecked && (userData.id ? <label>userData.username</label> : <label>Not signed in</label>)}
+    </div>
+  );
 }
 
 export default App;
