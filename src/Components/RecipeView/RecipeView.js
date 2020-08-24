0d6dcf78a3cffb86c1cf592720608d08a61204de
raiguard <caleb.heuer@gmail.com>
2020-08-24 14:40:06 -0600

    Prettier, clean up useEffects

--- src/Components/RecipeView/RecipeView.js
+++ src/Components/RecipeView/RecipeView.js
@@ -1,61 +1,48 @@
-import React, { useState, useEffect, useContext } from "react";
-import { RecipeIdContext } from "../../contexts/RecipeIdContext";
-import { MealPlanIdContext } from '../../contexts/MealPlanIdContext';
 import Axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { MealPlanIdContext } from "../../contexts/MealPlanIdContext";
+import { RecipeIdContext } from "../../contexts/RecipeIdContext";
 import "./RecipeView.scss";
 
 function RecipeView() {
   const [recipe, setRecipe] = useState([{}]);
   const { recipeId } = useContext(RecipeIdContext);
   const [mealPlanId, setMealPlanId] = useContext(MealPlanIdContext);
-  const [day, setDay] = useState('Sunday');
-  const [time, setTime] = useState('Breakfast');
+  const [day, setDay] = useState("Sunday");
+  const [time, setTime] = useState("Breakfast");
 
   //set recipe information to display
   useEffect(() => {
-    displayRecipe()
+    const fetchData = async () => {
+      const res = await Axios.get(`/api/recipe/${recipeId}`);
+      setRecipe(res.data);
+    };
+    fetchData();
   }, [recipeId]);
 
-    //retrieve recipe data from API
-    const displayRecipe = () => {
-      Axios.get(`/api/recipe/${recipeId}`)
-        .then((res) => {
-          setRecipe(res.data);
-        })
-        .catch((err) => console.log(err));
+  const handleAdd = () => {
+    const addPost = async () => {
+      await Axios.post("/api/add-recipe", { recipeId, mealPlanId, day, time, title: recipe[0].title });
+      setTime("Breakfast");
+      setDay("Sunday");
     };
+    addPost();
+  };
 
-    const handleAdd = () => {
-      Axios.post('/api/add-recipe', {recipeId, mealPlanId, day, time, title: recipe[0].title})
-      .then( () => {
-        setTime('Breakfast')
-        setDay('Sunday')
-      })
-      .catch(err => console.log(err))
-    }
-
-  console.log(time)
   return (
     <div className="recipe-display">
       <div className="recipe-view-container">
-        {/* <div>
-          <h1 style={{color: 'black'}}>Recipe View</h1>
-        </div> */}
         {!recipe[0].extendedIngredients ? (
           <h1>Loading</h1>
         ) : (
           <div className="recipe-info">
             <section className="recipe-directions">
-            <h1>{recipe[0].title}</h1>
+              <h1>{recipe[0].title}</h1>
               <img className="recipe-pic" src={recipe[0].image} alt="food" />
               {recipe[0].extendedIngredients.map((amount, i) => (
                 <section>
-                  <span className="ingredient-amount">
-                    {amount.measures.us.amount}
-                  </span>
-                  <span className="ingredient-measurement">
-                    {amount.measures.us.unitShort}
-                  </span>
+                  <span className="ingredient-amount">{amount.measures.us.amount}</span>
+                  <span className="ingredient-measurement">{amount.measures.us.unitShort}</span>
                   <span className="ingredient-name">{amount.name}</span>
                 </section>
               ))}
@@ -73,7 +60,7 @@ function RecipeView() {
       </div>
       <div className="seclector-area">
         <div className="selectors">
-          <select className="options" value={day} onChange={e => setDay(e.target.value)}>
+          <select className="options" value={day} onChange={(e) => setDay(e.target.value)}>
             <option>Sunday</option>
             <option>Monday</option>
             <option>Tuesday</option>
@@ -84,7 +71,7 @@ function RecipeView() {
           </select>
         </div>
         <div className="selectors">
-          <select className="options" value={time} onChange={e => setTime(e.target.value)}>
+          <select className="options" value={time} onChange={(e) => setTime(e.target.value)}>
             <option>Breakfast</option>
             <option>Lunch</option>
             <option>Dinner</option>
