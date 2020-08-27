6e4207b6c114a1807295db02cd3733fb19408f67
raiguard <caleb.heuer@gmail.com>
2020-08-27 16:12:40 -0600

    Better cart styling

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,10 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./Cart.scss";
 
 export default function Cart(props) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(null);
   const params = useParams();
 
   useEffect(() => {
@@ -15,25 +16,33 @@ export default function Cart(props) {
   }, [params.id]);
 
   return (
-    <table className="recipe-ingredients-table">
-      <tr>
-        <th></th>
-        <th>Aisle</th>
-        <th>Amount</th>
-        <th>Name</th>
-      </tr>
-      {cart.map((item) => (
-        <tr>
-          <td>
-            <img src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`} alt="Item" />
-          </td>
-          <td>{item.aisle}</td>
-          <td>
-            {item.amount} {item.measure}
-          </td>
-          <td>{item.name}</td>
-        </tr>
-      ))}
-    </table>
+    <main className="cart-container">
+      {cart ? (
+        <div className="cart-card">
+          <table className="cart-table">
+            <tr>
+              <th></th>
+              <th>Aisle</th>
+              <th>Amount</th>
+              <th>Name</th>
+            </tr>
+            {cart.map((item) => (
+              <tr>
+                <td>
+                  <img src={`https://spoonacular.com/cdn/ingredients_100x100/${item.image}`} alt="Item" />
+                </td>
+                <td>{item.aisle}</td>
+                <td>
+                  {item.amount} {item.measure}
+                </td>
+                <td>{item.name}</td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      ) : (
+        <h1>Loading cart, this might take a while...</h1>
+      )}
+    </main>
   );
 }
